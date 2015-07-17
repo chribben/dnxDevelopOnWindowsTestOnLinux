e24e5be73ee5919a986f8e059d847082cae1dc05
Christian Jacobsen <chribben@gmail.com>
2015-07-17 23:04:03 +0200

    Restore application under test

--- dnxTestRunner/Startup.cs
+++ dnxTestRunner/Startup.cs
@@ -2,7 +2,7 @@ using System;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
-
+using System.Diagnostics;
 namespace TestRunner
 {
     public class Startup
@@ -10,11 +10,24 @@ namespace TestRunner
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            RestoreApplicationUnderTest();
         }
 
         public void Configure(IApplicationBuilder app)
         {
             app.UseMvc();
+        }
+        private void RestoreApplicationUnderTest()
+        {
+            Console.WriteLine("Restoring application to test");
+            ProcessStartInfo psi = new ProcessStartInfo();
+            Process p = new Process();
+            psi.FileName = "dnu";
+            psi.Arguments = "restore /dnxApplication";
+            psi.UseShellExecute = false;
+            p.StartInfo = psi;
+            p.Start();
+            p.WaitForExit();
         }
     }
 }
